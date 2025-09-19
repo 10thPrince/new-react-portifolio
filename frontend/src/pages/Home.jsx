9ab0a1bd26082fe31c607a74ef7fa1f4e7e11914
10thPrince <ntwariprince5050@gmail.com>
2025-09-19 23:42:51 +0200

    finished the projects section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import About from '../components/About'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 const Home = () => {
   return (
@@ -20,6 +21,7 @@ const Home = () => {
             <HeroSection />
             <About />
             <Skills />
+            <Projects />
           </main>
         {/* Footer */}
     </div>
